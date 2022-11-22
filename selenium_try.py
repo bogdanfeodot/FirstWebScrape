47ffedd8daae377cd9cbe6ceb5800e138f583216
bogdanfeodot <feodotbogdan@gmail.com>
2022-11-22 20:28:39 +0200

    initial

---
+++ selenium_try.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Chrome(executable_path='D:/Python projects/Web Scraping Airplanes/chromedriver')
